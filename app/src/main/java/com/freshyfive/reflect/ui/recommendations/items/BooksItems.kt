ba0696023c56b4e38f3a4f39d6306d3f97843062
Swastik <guptaswastik7305@gmail.com>
2024-08-24 23:43:19 +0530

    Recommendations Layout

---
+++ app/src/main/java/com/freshyfive/reflect/ui/recommendations/items/BooksItems.kt
@@ -0,0 +1,21 @@
+package com.freshyfive.reflect.ui.recommendations.items
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.freshyfive.reflect.R
+
+class BooksItems : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_books_items)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
