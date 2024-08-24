acf98fa2e9829645c6c19a79928f75518fc7c70f
jinxedbuffer <srijandhak04@gmail.com>
2024-08-24 15:43:31 +0530

    ---

--- app/src/main/java/com/freshyfive/reflect/ui/HomeActivity.kt
+++ app/src/main/java/com/freshyfive/reflect/ui/HomeActivity.kt
@@ -1,11 +1,8 @@
 package com.freshyfive.reflect.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
