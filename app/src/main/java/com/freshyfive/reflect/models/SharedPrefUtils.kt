a3c5cacce2a62f0222b45976648dcbb201d9fc8f
Ritaban Chaudhuri <inrcr27@gmail.com>
2024-08-24 12:41:07 +0530

    Added SharedPrefUtils

---
+++ app/src/main/java/com/freshyfive/reflect/models/SharedPrefUtils.kt
@@ -0,0 +1,33 @@
+package com.freshyfive.reflect.models
+
+import android.content.Context
+import android.content.SharedPreferences
+
+fun saveUserData(context: Context, name: String, gender: String, profession: String) {
+    val sharedPreferences: SharedPreferences = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+    val editor = sharedPreferences.edit()
+    editor.putString("userName", name)
+    editor.putString("userGender", gender)
+    editor.putString("userProfession", profession)
+    editor.apply()
+}
+
+fun saveMood(context: Context,mood:String)
+{
+    val sharedPreferences: SharedPreferences = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+    val editor = sharedPreferences.edit()
+    editor.putString("userMood",mood)
+    editor.apply()
+}
+
+fun getUserName(context: Context): String? {
+    val sharedPreferences: SharedPreferences = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+    return sharedPreferences.getString("userName", null)
+}
+
+fun isUserDataAvailable(context: Context): Boolean {
+    val sharedPreferences: SharedPreferences = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+    return sharedPreferences.contains("userName") &&
+            sharedPreferences.contains("userGender") &&
+            sharedPreferences.contains("userProfession")
+}
