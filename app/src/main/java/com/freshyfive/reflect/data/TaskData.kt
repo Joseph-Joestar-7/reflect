f321a44f96630c4961ccec2e7ecdcfbfd2d5b807
Ritaban Chaudhuri <inrcr27@gmail.com>
2024-08-25 10:36:54 +0530

    Added Daily Task

---
+++ app/src/main/java/com/freshyfive/reflect/data/TaskData.kt
@@ -0,0 +1,149 @@
+package com.freshyfive.reflect.data
+
+val tasksStudentAngry: Set<String> = setOf(
+    "Practice Deep Breathing",
+    "Exercise for 15 mins",
+    "Use Positive Affirmations",
+    "Practice Meditation",
+    "Take a Timeout"
+)
+
+val tasksStudentSad: Set<String> = setOf(
+    "Exercise Regularly: Engage in activities like jogging, cycling, or playing sports. Physical exercise boosts endorphins, which can improve mood and reduce stress.",
+    "Set Daily Goals: Create a list of achievable goals for the day. Completing tasks can provide a sense of accomplishment and motivation.",
+    "Practice Mindfulness or Meditation: Spend a few minutes each day practicing mindfulness or meditation to calm the mind and reduce anxiety.",
+    "Connect with Friends: Make time to interact with friends, whether through in-person meetups, video calls, or social media. Positive social interactions can enhance mood.",
+    "Engage in a Creative Hobby: Pursue creative activities such as drawing, writing, or playing a musical instrument. Creative expression can be therapeutic and fulfilling."
+)
+
+val tasksStudentAnxious: Set<String> = setOf(
+    "Set Up a Study Routine: Create a structured study schedule with regular breaks. Breaking study sessions into manageable chunks and having a clear plan can reduce feelings of being overwhelmed.",
+    "Engage in Physical Activity: Incorporate daily exercise, such as jogging, yoga, or sports. Physical activity helps release endorphins, which can improve mood and reduce anxiety.",
+    "Practice Gratitude: Start or end each day by writing down three things you are grateful for. Focusing on positive aspects of life can shift your mindset and reduce stress.",
+    "Explore Creative Arts: Spend time engaging in creative activities like drawing, painting, or crafting. Creative expression can be a calming and enjoyable way to unwind.",
+    "Try Mindfulness Meditation: Dedicate a few minutes each day to mindfulness meditation or guided imagery. This can help center your thoughts and manage anxiety effectively.",
+    "Connect with Friends: Reach out to friends for a quick chat or hang out. Socializing, even briefly, can provide support and reduce feelings of isolation."
+)
+
+val tasksStudentDepressed: Set<String> = setOf(
+    "Establish a morning routine",
+    "Engage in physical activity",
+    "Set small, achievable goals",
+    "Practice mindfulness or meditation",
+    "Stay connected with friends"
+)
+
+val tasksStudentFrustrated: Set<String> = setOf(
+    "Practice Mindfulness: Spend 5-10 minutes daily focusing on your breath.",
+    "Exercise Regularly: Engage in at least 30 minutes of physical activity.",
+    "Create a To-Do List: Organize tasks to stay on track and reduce overwhelm.",
+    "Take Regular Breaks: Use techniques like the Pomodoro Technique for efficient studying.",
+    "Stay Hydrated: Drink plenty of water throughout the day."
+)
+
+val tasksStudentWorried: Set<String> = setOf(
+    "Practice deep breathing",
+    "Write in a journal",
+    "Set small, achievable goals",
+    "Limit social media time",
+    "Engage in physical activity"
+)
+
+val tasksWorkingAngry: Set<String> = setOf(
+    "Morning Meditation (5-10 minutes)",
+    "Physical Exercise",
+    "Mindful Moments",
+    "Time Management",
+    "Healthy Eating Habits"
+)
+
+val tasksWorkingSad: Set<String> = setOf(
+    "Plan a Weekly Project: Start a small, enjoyable project that you can work on daily, such as a DIY home improvement, a craft, or a personal blog. Having something to look forward to can be very motivating.",
+    "Explore a New Skill or Course: Dedicate time to learning something new, such as cooking a new recipe, trying a new language, or taking an online course. Lifelong learning can be fulfilling and invigorating.",
+    "Take a Nature Walk: Spend time in nature each day, even if it's just a short walk in a nearby park. Being outdoors can improve mood and provide a refreshing break from routine.",
+    "Write a Daily Reflection: Start a daily journal where you reflect on your experiences, thoughts, and feelings. This practice can help process emotions and gain clarity.",
+    "Create a Vision Board: Spend time creating a vision board with images and words that represent your goals and aspirations. Visualizing your dreams can inspire and motivate you."
+)
+
+val tasksWorkingAnxious: Set<String> = setOf(
+    "Adopt Healthy Eating Habits: Focus on balanced, nutritious meals and snacks throughout the day. Maintaining healthy eating habits can stabilize energy levels and improve mood.",
+    "Set Boundaries for Work-Life Balance: Establish clear boundaries between work and personal time. Avoid checking work emails or taking calls after hours to maintain a healthy work-life balance.",
+    "Practice Mindful Listening: When interacting with colleagues or clients, practice mindful listening by fully focusing on the speaker. Active listening can improve communication and reduce misunderstandings that cause stress.",
+    "Engage in Daily Relaxation Exercises: Integrate relaxation exercises such as progressive muscle relaxation or guided imagery into your daily routine. These practices can help manage anxiety and promote relaxation.",
+    "Pursue a Personal Project or Passion: Dedicate time to a personal project or hobby outside of work. Engaging in activities you enjoy can provide a sense of fulfillment and a break from work-related stress."
+)
+
+val tasksWorkingDepressed: Set<String> = setOf(
+    "Establish a morning routine",
+    "Engage in physical activity",
+    "Set small, manageable work goals",
+    "Practice mindfulness or meditation",
+    "Stay connected with colleagues and friends"
+)
+
+val tasksWorkingFrustrated: Set<String> = setOf(
+    "Prioritize Tasks: Focus on high-priority items first.",
+    "Practice Mindfulness: Spend 5-10 minutes on mindfulness meditation.",
+    "Take Short Walks: Incorporate brief walks during breaks.",
+    "Set Clear Boundaries: Separate work time from personal time.",
+    "Stay Organized: Keep your workspace tidy and manage tasks effectively."
+)
+
+val tasksWorkingWorried: Set<String> = setOf(
+    "Start the Day with a Morning Routine",
+    "Set Clear Priorities and Goals",
+    "Take Short, Regular Breaks",
+    "Practice Deep Breathing or Meditation",
+    "Limit Caffeine Intake"
+)
+
+
+val tasksRetiredAngry: Set<String> = setOf(
+    "Practice Mindfulness or Meditation",
+    "Engage in Physical Activity",
+    "Keep a Journal",
+    "Develop a Routine",
+    "Practice Gratitude"
+)
+
+val tasksRetiredSad: Set<String> = setOf(
+    "Engage in Physical Exercise: Daily activities like walking, stretching, or gentle yoga can improve physical health, increase energy levels, and enhance mood.",
+    "Pursue a Hobby: Dedicate time to activities such as gardening, painting, knitting, or woodworking. Hobbies provide a creative outlet and a sense of accomplishment.",
+    "Read a Book: Reading, whether fiction or non-fiction, can be both entertaining and intellectually stimulating. It offers an escape and can be a source of inspiration.",
+    "Connect with Loved Ones: Regularly reach out to family and friends through phone calls, video chats, or visits. Social connections are vital for emotional well-being.",
+    "Volunteer: Consider volunteering for local organizations or community groups. Helping others can provide a sense of purpose and fulfillment."
+)
+
+val tasksRetiredAnxious: Set<String> = setOf(
+    "Gardening: Engage in light gardening or tending to indoor plants. The act of nurturing plants and being outdoors can be calming and provide a sense of accomplishment.",
+    "Creative Writing: Start a journal, write poetry, or compose short stories. Expressing thoughts and feelings through writing can be therapeutic and help process emotions.",
+    "Cooking New Recipes: Try preparing new and healthy recipes. Cooking can be a relaxing activity and experimenting with new dishes can bring joy and satisfaction.",
+    "Volunteering: Offer time or skills to local charities or community organizations. Volunteering provides a sense of purpose and connection to others.",
+    "Photography: Take up photography as a hobby. Capturing moments and scenes can be a creative outlet and encourage you to appreciate the beauty around you."
+)
+
+val tasksRetiredDepressed: Set<String> = setOf(
+    "Establish a morning routine",
+    "Engage in physical activity",
+    "Set small, achievable goals",
+    "Practice mindfulness or meditation",
+    "Stay connected with friends and family"
+)
+
+val tasksRetiredFrustrated: Set<String> = setOf(
+    "Practice Tai Chi: Engage in gentle, flowing exercises to improve balance and reduce stress.",
+    "Volunteer: Contribute your time to local charities or community organizations.",
+    "Join a Club: Participate in social clubs or groups related to your interests.",
+    "Cook or Bake: Try new recipes or prepare favorite dishes for enjoyment.",
+    "Listen to Music: Create playlists of your favorite tunes to uplift your mood."
+)
+
+val tasksRetiredWorried: Set<String> = setOf(
+    "Establish a Morning Routine",
+    "Engage in a Hobby",
+    "Take Daily Walks or Exercise",
+    "Practice Mindfulness or Meditation",
+    "Connect with Friends or Family"
+)
+
+
--- app/src/main/java/com/freshyfive/reflect/ui/dailytasks/DailyTasksFragment.kt
+++ app/src/main/java/com/freshyfive/reflect/ui/dailytasks/DailyTasksFragment.kt
@@ -1,22 +1,33 @@
 package com.freshyfive.reflect.ui.dailytasks
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.freshyfive.reflect.R
+import com.freshyfive.reflect.data.retiredtaskMap
+import com.freshyfive.reflect.data.studenttaskMap
+import com.freshyfive.reflect.data.workingtaskMap
+import com.freshyfive.reflect.models.getMood
+import com.freshyfive.reflect.models.getProfession
 
 class DailyTasksFragment : Fragment() {
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var tasksCompletedTextView: TextView
+//    private lateinit var totalTasksTextView: TextView
 
     companion object {
         fun newInstance() = DailyTasksFragment()
     }
 
-    private val viewModel: DailyTasksViewModel by viewModels()
+    private var completedTasks :Int=0
+    private val totalTasks :Int=3
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,14 +38,114 @@ class DailyTasksFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        sharedPreferences = view.context.getSharedPreferences("DailyTaskPrefs", Context.MODE_PRIVATE)
 
         val taskRecyclerView = view.findViewById<RecyclerView>(R.id.dailyTasksView)
+        tasksCompletedTextView = view.findViewById(R.id.tasksCompletedTextView)
+//        totalTasksTextView = view.findViewById(R.id.totalTasksTextView)
 
-        val list = listOf(
-                "Go for a walk", "Meditate"
-        )
 
-        taskRecyclerView.adapter = DailyTaskAdapter(list)
+        val list = getDailyTasks() ?: emptyList()
+
+        taskRecyclerView.adapter = DailyTaskAdapter(list) {
+            completeTask()
+        }
         taskRecyclerView.layoutManager = LinearLayoutManager(view.context)
+        completedTasks = getCompletedTasksCount()
+        updateTaskCounts()
+
     }
+
+    private fun getTasks(mood: String?, profession: String?): List<String>? {
+        when (profession) {
+            "Student" -> {
+                return studenttaskMap[mood]?.shuffled()?.take(3)
+            }
+
+            "Professional Working Class" -> {
+                return workingtaskMap[mood]?.shuffled()?.take(3)
+            }
+
+            "Retired" -> {
+                return retiredtaskMap[mood]?.shuffled()?.take(3)
+            }
+
+            else -> {
+                return emptyList()
+            }
+        }
+    }
+
+    private fun completeTask() {
+        if (completedTasks < totalTasks) {
+            incrementCompletedTasks()
+            addUserPoint()
+            updateTaskCounts()
+        }
+    }
+
+    private fun incrementCompletedTasks() {
+        completedTasks = getCompletedTasksCount()
+        sharedPreferences.edit()
+            .putInt("completedTasks", completedTasks + 1)
+            .apply()
+    }
+
+    private fun getDailyTasks(): List<String>? {
+        val lastUpdated = sharedPreferences.getLong("lastUpdated", 0L)
+        val currentTime = System.currentTimeMillis()
+
+        // Check if 24 hours have passed since the last update
+        return if (currentTime - lastUpdated >= 24 * 60 * 60 * 1000) {
+            // Select 3 random tasks from the available tasks
+            val newTasks = getTasks(getMood(requireContext())?.uppercase(), getProfession(requireContext()))
+                ?.shuffled()
+                ?.take(3)
+
+            // Save the new tasks and update the timestamp
+            sharedPreferences.edit()
+                .putStringSet("dailyTasks", newTasks?.toSet())
+                .putInt("completedTasks", 0) // Reset completed tasks count
+                .putLong("lastUpdated", currentTime) // Update last updated time
+                .apply()
+
+            newTasks
+        } else {
+            sharedPreferences.getStringSet("dailyTasks", emptySet())?.toList()
+        }
+    }
+
+    fun getCompletedTasksCount(): Int {
+        return sharedPreferences.getInt("completedTasks", 0)
+    }
+
+    private fun updateTaskCounts() {
+        val completedTasks = getCompletedTasksCount()
+        val totalTasks = getTotalTasksCount()
+        tasksCompletedTextView.text = "Tasks Completed: $completedTasks / $totalTasks"
+    }
+
+//    fun completeTask() {
+//        val completedTasks = sharedPreferences.getInt("completedTasks", 0)
+//        sharedPreferences.edit()
+//            .putInt("completedTasks", completedTasks + 1)
+//            .apply()
+//    }
+
+    fun getTotalTasksCount(): Int {
+        return sharedPreferences.getStringSet("dailyTasks", emptySet())!!.size
+    }
+
+    fun getUserPoints(): Int {
+        return sharedPreferences.getInt("userPoints", 0)
+    }
+
+    fun addUserPoint() {
+        val userPoints = getUserPoints()
+        sharedPreferences.edit()
+            .putInt("userPoints", userPoints + 1)
+            .apply()
+    }
+
+
 }
